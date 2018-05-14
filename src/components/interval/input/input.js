a2e096b18be8880afa5f92ea10d1082fc2710579
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2018-05-14 12:23:51 +0530

    align components styling

--- src/components/interval/input/input.js
+++ src/components/interval/input/input.js
@@ -5,9 +5,9 @@ import classes from './input.css';
 const Input = (props) => {
 	return (
 		<div className={classes.Input}>
-			<input type="text" value={props.leftValue} onChange={props.onLeftChange} />
-			<span>:</span>
-			<input type="text" value={props.rightValue} onChange={props.onRightChange} />
+			<input className={classes.field} type="text" value={props.leftValue} onChange={props.onLeftChange} />
+			<span className={classes.separator}>:</span>
+			<input className={classes.field} type="text" value={props.rightValue} onChange={props.onRightChange} />
 		</div>
 	);
 };
