92ecea25f2215194d499b8b0928321f08f3529c5
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2018-06-05 21:02:06 +0530

    fix - vertical center app with scrolling

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import ReactDOM from 'react-dom';
 import App from './components/app/app.js';
 
 import './reset.css';
+import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
