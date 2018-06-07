d15a28560d442f289a11a3c4086f4e5f402f64a3
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2018-06-07 15:58:25 +0530

    header component, meta description; remove src/ and all meta files

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import App from './components/app/app.js';
-
-import './reset.css';
-import './index.css';
-
-ReactDOM.render(<App />, document.getElementById('root'));
